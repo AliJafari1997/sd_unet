27b96c70f8bb42e18463956e8f564ac75fab18d8
Ali Jafari <jafariali694@gmail.com>
2022-10-08 02:33:52 +0330

    Add files via upload

---
+++ data.py
@@ -0,0 +1,202 @@
+
+import os
+import numpy as np
+import cv2
+from glob import glob
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,
+    CenterCrop,
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    RandomGamma,
+    HueSaturationValue,
+    RGBShift,
+    RandomBrightness,
+    RandomContrast,
+    MotionBlur,
+    MedianBlur,
+    GaussianBlur,
+    GaussNoise,
+    ChannelShuffle,
+    CoarseDropout, 
+    Rotate
+)
+
+def create_dir(path):
+    """ Create a directory. """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def load_data(path, split=0.2):
+    """ Load the images and masks """
+    images = sorted(glob(f"{path}/*/image/*.png"))
+    masks = sorted(glob(f"{path}/*/mask/*.png"))
+
+    """ Split the data """
+    split_size = int(len(images) * split)
+    train_x, valid_x = train_test_split(images, test_size=split_size, random_state=42)
+    train_y, valid_y = train_test_split(masks, test_size=split_size, random_state=42)
+
+    return (train_x, train_y), (valid_x, valid_y)
+
+def augment_data(images, masks, save_path, augment=True):
+    """ Performing data augmentation. """
+    H = 512
+    W = 512
+
+    for idx, (x, y) in tqdm(enumerate(zip(images, masks)), total=len(images)):
+        """ Extracting the dir name and image name """
+        dir_name = x.split("/")[-3]
+        name = dir_name + "_" + x.split("/")[-1].split(".")[0]
+
+        """ Read the image and mask """
+        x = cv2.imread(x, cv2.IMREAD_COLOR)
+        y = cv2.imread(y, cv2.IMREAD_COLOR)
+
+        x = cv2.resize(x, (H, W))
+        y = cv2.resize(y, (H, W))
+
+        if augment == True:
+            aug = HorizontalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x1 = augmented["image"]
+            y1 = augmented["mask"]
+
+            aug = VerticalFlip(p=1)
+            augmented = aug(image=x, mask=y)
+            x2 = augmented['image']
+            y2 = augmented['mask']
+
+            aug = Rotate(limit=45, p=1.0)
+            augmented = aug(image=x, mask=y)
+            x3 = augmented["image"]
+            y3 = augmented["mask"]
+
+
+            aug = Transpose(p=1)
+            augmented = aug(image=x, mask=y)
+            x4 = augmented['image']
+            y4 = augmented['mask']
+
+            ## ElasticTransform
+            aug = ElasticTransform(p=1, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
+            augmented = aug(image=x, mask=y)
+            x5 = augmented['image']
+            y5 = augmented['mask']
+
+            ## Grid Distortion
+            aug = GridDistortion(p=1)
+            augmented = aug(image=x, mask=y)
+            x6 = augmented['image']
+            y6 = augmented['mask']
+
+            ## Optical Distortion
+            aug = OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)
+            augmented = aug(image=x, mask=y)
+            x7 = augmented['image']
+            y7 = augmented['mask']
+
+            aug = RandomBrightnessContrast(p=1)
+            augmented = aug(image=x, mask=y)
+            x8 = augmented['image']
+            y8 = augmented['mask']
+
+            aug = RandomGamma(p=1)
+            augmented = aug(image=x, mask=y)
+            x9 = augmented['image']
+            y9 = augmented['mask']
+
+            aug = HueSaturationValue(p=1)
+            augmented = aug(image=x, mask=y)
+            x10 = augmented['image']
+            y10 = augmented['mask']
+
+            aug = RGBShift(p=1)
+            augmented = aug(image=x, mask=y)
+            x11 = augmented['image']
+            y11 = augmented['mask']
+
+            aug = RandomBrightness(p=1)
+            augmented = aug(image=x, mask=y)
+            x12 = augmented['image']
+            y12 = augmented['mask']
+
+            aug = RandomContrast(p=1)
+            augmented = aug(image=x, mask=y)
+            x13 = augmented['image']
+            y13 = augmented['mask']
+
+            aug = MotionBlur(p=1, blur_limit=7)
+            augmented = aug(image=x, mask=y)
+            x14 = augmented['image']
+            y14 = augmented['mask']
+
+            aug = MedianBlur(p=1, blur_limit=10)
+            augmented = aug(image=x, mask=y)
+            x15 = augmented['image']
+            y15 = augmented['mask']
+
+            X = [
+                x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10,
+                x11, x12, x13, x14, x15
+            ]
+            Y = [
+            y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10,
+            y11, y12, y13, y14, y15
+            ]
+
+        else:
+            X = [x]
+            Y = [y]
+
+        idx = 0
+        for i, m in zip(X, Y):
+            i = cv2.resize(i, (W, H))
+            m = cv2.resize(m, (W, H))
+            m = m/255.0
+            m = (m > 0.5) * 255      # just 0 and 255
+
+            if len(X) == 1:  # there is not no augmentation for validation data
+                tmp_image_name = f"{name}.jpg"
+                tmp_mask_name  = f"{name}.jpg"
+
+            else:         # augmented training data
+                tmp_image_name = f"{name}_{idx}.jpg"
+                tmp_mask_name  = f"{name}_{idx}.jpg"
+            # saving images and masks after data augmentation in the defined path
+            image_path = os.path.join(save_path, "image/", tmp_image_name)
+            mask_path  = os.path.join(save_path, "mask/", tmp_mask_name)
+
+            cv2.imwrite(image_path, i)
+            cv2.imwrite(mask_path, m)
+
+            idx += 1
+
+if __name__ == "__main__":
+    """ Load the dataset """
+    dataset_path = os.path.join("data", "train")
+    (train_x, train_y), (valid_x, valid_y) = load_data(dataset_path, split=0.2)
+
+    print("Train: ", len(train_x))
+    print("Valid: ", len(valid_x))
+
+    create_dir("new_data/train/image/")
+    create_dir("new_data/train/mask/")
+    create_dir("new_data/valid/image/")
+    create_dir("new_data/valid/mask/")
+
+    augment_data(train_x, train_y, "new_data/train/", augment=True)
+    augment_data(valid_x, valid_y, "new_data/valid/", augment=False)
---
+++ model.py
@@ -0,0 +1,97 @@
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPooling2D, Conv2DTranspose, Concatenate, Input, AveragePooling2D, UpSampling2D
+from tensorflow.keras.models import Model
+import tensorflow as tf
+
+def conv_block(input, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(input)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    return x
+
+def SqueezeAttentionBlock(input, num_filters):
+    x_res = conv_block(input, num_filters)
+
+    y = AveragePooling2D((2, 2))(input)
+    y = conv_block(y, num_filters)
+    y = UpSampling2D(size=(2, 2))(y)
+
+    return (y * x_res) + y
+
+def encoder_block(input, num_filters):
+    x = SqueezeAttentionBlock(input, num_filters)
+    max_pool = MaxPooling2D((2, 2))(x)
+    avg_pool = AveragePooling2D((2, 2))(x)
+    cat = Concatenate()([max_pool, avg_pool])
+    output = Conv2D(num_filters, (1, 1), padding='same')(cat)
+    return x, output
+
+
+def dense_aspp(x, filter):
+    shape = x.shape
+
+    # conv1*1 -> BN -> ReLU
+    conv_1 = Conv2D(filter, 1, padding="same", use_bias=False)(x)
+    conv_1 = BatchNormalization()(conv_1)
+    conv_1 = Activation("relu")(conv_1)
+
+    # Conv3*3 with dilation_rate=6 -> BN -> ReLU
+    y0 = Conv2D(filter, 3, padding="same", dilation_rate=(6, 6), use_bias=False)(x)
+    y0 = BatchNormalization()(y0)
+    y0 = Activation("relu")(y0) 
+
+    concat_x_y0 = Concatenate()([x, y0])
+    y1 = Conv2D(filter, 3, padding="same", dilation_rate=(12, 12), use_bias=False)(concat_x_y0)
+    y1 = BatchNormalization()(y1)
+    y1 = Activation("relu")(y1)
+
+    concat_x_y0_y1 = Concatenate()([x, y0, y1])
+    y2 = Conv2D(filter, 3, padding="same", dilation_rate=(18, 18), use_bias=False)(concat_x_y0_y1)
+    y2 = BatchNormalization()(y2)
+    y2 = Activation("relu")(y2)  
+
+    avg_pool = AveragePooling2D((2, 2))(x)
+    avg_pool = UpSampling2D((2, 2))(avg_pool)
+    y = Concatenate()([conv_1, y0, y1, y2, avg_pool])
+    y = Conv2D(filter, 1, dilation_rate=1, padding="same", use_bias=False)(y)
+    y = BatchNormalization()(y)
+    y = Activation("relu")(y)
+
+    return y
+
+def decoder_block(input, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
+
+
+def build_model(input_shape):
+    inputs = Input(input_shape)
+
+    skip1, out1 = encoder_block(inputs, 32)
+    skip2, out2 = encoder_block(out1, 64)
+    skip3, out3 = encoder_block(out2, 128)
+    skip4, out4 = encoder_block(out3, 256)
+    out5 = SqueezeAttentionBlock(out4, 512)
+
+    aspp_output = dense_aspp(out5, 256)
+
+    d1 = decoder_block(aspp_output, skip4, 256)   # 256+256 -> 256
+    d2 = decoder_block(d1, skip3, 128)            # 128+128 -> 128
+    d3 = decoder_block(d2, skip2, 64)             # 64+64 -> 64
+    d4 = decoder_block(d3, skip1, 32)             # 32+32 -> 32
+
+    outputs = Conv2D(1, (3, 3), padding="same", activation="sigmoid")(d4)
+
+    model = Model(inputs, outputs, name="SD-UNet")
+    return model
+
+if __name__ == "__main__":
+    input_shape = (512, 512, 3)
+    model = build_model(input_shape)
+    model.summary()        
