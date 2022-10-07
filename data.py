ee74c6a77c7ce0658259c9f3494a5a3a66f6eb0f
Ali Jafari <jafariali694@gmail.com>
2022-10-08 02:47:47 +0330

    Update data.py

--- data.py
+++ data.py
@@ -144,18 +144,13 @@ def augment_data(images, masks, save_path, augment=True):
             x14 = augmented['image']
             y14 = augmented['mask']
 
-            aug = MedianBlur(p=1, blur_limit=10)
-            augmented = aug(image=x, mask=y)
-            x15 = augmented['image']
-            y15 = augmented['mask']
-
             X = [
                 x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10,
-                x11, x12, x13, x14, x15
+                x11, x12, x13, x14
             ]
             Y = [
             y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10,
-            y11, y12, y13, y14, y15
+            y11, y12, y13, y14
             ]
 
         else:
